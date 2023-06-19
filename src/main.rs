a800633dc3d5f3591644ab7947420fbc721f48c0
roesnera <adam.roesner@generation.org>
2023-06-19 18:38:35 -0400

    refactored to use generics and traits

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = gen_largest(&number_list);
     println!("The largest number is: {result}");
     
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let result = largest(&number_list);
+    let result = gen_largest(&number_list);
     println!("The largest number is: {result}");    
 }
 
@@ -18,6 +18,18 @@ fn largest(list: &[i32]) -> i32 {
             largest = *num;
         };
     };
+
+    largest
+}
+
+fn gen_largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest: &T = &list[0];
+
+    for el in list {
+        if el > &largest {
+            largest = el;
+        }
+    }
 
     largest
 }
