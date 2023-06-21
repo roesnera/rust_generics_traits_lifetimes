d08b7c8ced7ca65c9f78d9d29afd03731479b5a8
roesnera <adam.roesner@generation.org>
2023-06-21 16:22:07 -0400

    implements some simple traits

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,25 @@ fn main() {
 
     let result = gen_largest(&number_list);
     println!("The largest number is: {result}");    
+
+    let art: NewsArticle = NewsArticle{
+        headline: String::from("Submarine Missing: Pressure to Rescue Mounting"),
+        location: String::from("Titanic"),
+        author: String::from("Adam Roesner"),
+        content: String::from(". . ."),
+    };
+
+
+    summarize(&art);
+
+    let tweet: Tweet = Tweet {
+        username: String::from("Adam"),
+        content: String::from("Check out my article about that one submarine lol"),
+        reply: true,
+        retweet: true
+    };
+
+    summarize(&tweet);
 }
 
 fn largest(list: &[i32]) -> i32 {
@@ -32,4 +51,38 @@ fn gen_largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     }
 
     largest
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})",self.headline, self.author, self.location)
+    }
+}
+
+struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}",self.username, self.content)
+    }
+}
+
+fn summarize(thing: &dyn Summary) {
+    println!("{}",thing.summarize());
 }
